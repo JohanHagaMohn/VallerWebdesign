46ee449c9bb37f3259705c6d149233c6ac0b07e3
Johan Haga Mohn <47793383+JohanHagaMohn@users.noreply.github.com>
2020-02-01 19:55:34 +0100

    Added tweaks and accessibility

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-window.addEventListener("DOMContentLoaded", () => {
+function hamburger() {
     const input = document.querySelector("#menu").firstElementChild;
     let visibility = false
     input.addEventListener("click", () => {
@@ -11,4 +11,26 @@ window.addEventListener("DOMContentLoaded", () => {
             input.checked = visibility = false;
         }
     });
+    document.querySelectorAll("li").forEach(e => {
+        e.firstElementChild.addEventListener("click", () => {
+            if (visibility) {
+                document.querySelector("html").classList.toggle("hamburger");
+                input.checked = visibility = false;
+            }
+        })
+    })
+}
+function form() {
+    const submit = document.querySelector("#send").firstElementChild;
+    submit.addEventListener("click", e => {
+        e.preventDefault();
+        const message = document.querySelector("#melding").firstElementChild.value;
+        const email = document.querySelector("#email").firstElementChild.value;
+        window.open(`mailto:jmohn02@gmail.com?body=${message}&subject=${email}`, "_blank");
+    })
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    hamburger();
+    form();
 })
