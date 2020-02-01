4883e042001432de0ffce6ce24b616bb2934a3c2
Johan Haga Mohn <47793383+JohanHagaMohn@users.noreply.github.com>
2020-02-01 15:59:28 +0100

    Css Hamburger

--- index.js
+++ index.js
@@ -1,36 +1,14 @@
 window.addEventListener("DOMContentLoaded", () => {
-    const html = document.querySelector("html");
-    const nav = document.querySelector("nav");
-    const header = document.querySelector("header");
-    const logo = document.querySelector("#logo");
-    const li = document.querySelectorAll("li")
-    let menu = false;
-
-    function show() {
-        menu = true;
-        header.style.height = "100vh";
-        html.style.overflowY = "hidden";
-        logo.style.visibility = "hidden";
-    }
-    function hide() {
-        menu = false
-        header.style.height = "";
-        html.style.overflowY = "";
-        logo.style.visibility = "";
-    }
-
+    const input = document.querySelector("#menu").firstElementChild;
+    let visibility = false
+    input.addEventListener("click", () => {
+        document.querySelector("html").classList.toggle("hamburger");
+        visibility = !visibility;
+    })
     window.addEventListener("resize", function scrolling() {
-        if (menu && window.innerWidth > 640) {
-            hide()
+        if (visibility && window.innerWidth > 640) {
+            document.querySelector("html").classList.toggle("hamburger");
+            input.checked = visibility = false;
         }
     });
-    header.addEventListener("click", () => {
-        if (window.innerWidth < 640) {
-            if (!menu) {
-                show()
-            } else {
-                hide();
-            }
-        }
-    })
 })
